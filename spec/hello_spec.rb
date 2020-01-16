f85b5bc6902dc797367eea05a8ec511779d2d631
Bruno Silva <bruno-ap@hotmail.com>
2020-01-16 00:00:59 -0300

    Commit 16-01-2020

---
+++ spec/hello_spec.rb
@@ -0,0 +1,7 @@
+describe 'meu primeiro script' do
+    it 'visitar a página'do
+        visit 'https://training-wheels-protocol.herokuapp.com/'
+        puts page.title
+        expect(page.title).to eql 'Training Wheels Protocol'
+    end
+end
