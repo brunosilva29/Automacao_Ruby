92377d9355aa825e95cadf76156a1de7357dbf33
Bruno Silva <bruno-ap@hotmail.com>
2020-01-16 01:39:18 -0300

    Commit Login três

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -9,4 +9,22 @@ describe 'Login' do
         # expect(find('#flash').text).to include 'Olá, Tony Stark. Você acessou a área logada!'
         expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
     end
+
+    it 'Login com senha incorreta'do
+        visit 'https://training-wheels-protocol.herokuapp.com/login'
+        fill_in 'userId', with: 'stark'
+        fill_in 'passId', with:'teste'
+        click_button 'Login'
+
+        expect(find('#flash')).to have_content 'Senha é invalida!'
+    end
+
+    it 'usuário não cadastrado' do
+        visit 'https://training-wheels-protocol.herokuapp.com/login'
+        fill_in 'userId', with:'teste'
+        fill_in 'passId', with:'jarvis!'
+        click_button 'Login'
+
+        expect(find('#flash')).to have_content 'O usuário informado não está cadastrado!'
+    end
 end
