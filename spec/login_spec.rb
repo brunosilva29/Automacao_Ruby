d6a02143bd6e6a2494d42238d92c28ceef7c3ca9
Bruno Silva <bruno-ap@hotmail.com>
2020-01-16 01:01:30 -0300

    Commit Login dois

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -5,6 +5,8 @@ describe 'Login' do
         fill_in 'passId', with: 'jarvis!'
         click_button 'Login'
 
-        expect(find('#flash').visible?).to be true
+        # expect(find('#flash').visible?).to be true
+        # expect(find('#flash').text).to include 'Olá, Tony Stark. Você acessou a área logada!'
+        expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
     end
 end
